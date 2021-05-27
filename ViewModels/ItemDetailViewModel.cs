5d3d65c2938bbf225138dcf62a2201cd0a0e3f94
Calebyang93 <caleb.yanghannchih93@gmail.com>
2021-05-27 17:05:26 +0800

    This is the boilerplate code of the Xamarin Android IOS application for Xamarin Forms

---
+++ ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using XFAApp1.Models;
+
+namespace XFAApp1.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
